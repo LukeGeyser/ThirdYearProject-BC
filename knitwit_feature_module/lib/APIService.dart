950476a1340c84c09ef1ac271ad4a187b08240e2
Luke <geyserlg@gmail.com>
2020-12-02 11:14:01 +0200

    Added things

--- knitwit_feature_module/lib/APIService.dart
+++ knitwit_feature_module/lib/APIService.dart
@@ -4,16 +4,17 @@ import 'package:http/http.dart' as http;
 import 'package:knitwit_feature_module/PatternsModel.dart';
 
 class APIService {
-  Future<PatternsModel> fetchAlbum() async {
+  Future<dynamic> searchProject(String searchQuery) async {
     var bytes = utf8.encode(
         "read-92f68094886a814f12978f1547026470:SbnQIzMLAPWrUemqJIP2o22Xs+QFAtuUnUFBbLSK");
     var base64Str = base64.encode(bytes);
     final response = await http.get(
-      'https://api.ravelry.com/patterns.json?ids=600+601',
+      'https://api.ravelry.com/patterns/search.json?query=' +
+          searchQuery +
+          ";page_size=20",
       headers: {HttpHeaders.authorizationHeader: "Basic " + base64Str},
     );
     final responseJson = jsonDecode(response.body);
-
-    return PatternsModel.fromJson(responseJson);
+    return responseJson;
   }
 }
--- knitwit_feature_module/lib/main.dart
+++ knitwit_feature_module/lib/main.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:knitwit_feature_module/APIService.dart';
+import 'package:social_share/social_share.dart';
+import 'package:social_share_plugin/social_share_plugin.dart';
 
 APIService service = new APIService();
 
@@ -10,51 +15,62 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("KnitWit Module Test"),
-        ),
-        body:
-        Container(
-          child:
-          Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-        Expanded(
-          child: RaisedButton(
-            color: Colors.white38,
-              onPressed: () {},
-          child: const Text('Share To Facebook', style: TextStyle(fontSize: 20)),
-        )
-      ),
-                Expanded(
-                    child: RaisedButton(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("KnitWit Module Test"),
+            ),
+            body: Container(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Expanded(
+                        child: RaisedButton(
+                      color: Colors.white38,
+                      onPressed: () {},
+                      child: const Text('Share To Facebook',
+                          style: TextStyle(fontSize: 20)),
+                    )),
+                    Expanded(
+                        child: RaisedButton(
                       color: Colors.lightBlue,
                       onPressed: () {},
-
-                      child: const Text('Search Ravelry', style: TextStyle(fontSize: 20)),
-                    )
-                ),
-                Expanded(
-                    child: RaisedButton(
+                      child: const Text('Search Ravelry',
+                          style: TextStyle(fontSize: 20)),
+                    )),
+                    Expanded(
+                        child: RaisedButton(
                       color: Colors.cyan,
-                      onPressed: () {},
-                      child: const Text('Add To Project', style: TextStyle(fontSize: 20)),
-                    )
-                ),
-                Expanded(
-                    child: RaisedButton(
+                      onPressed: () async {
+                        // var data = await service.searchProject("cowl");
+
+                        // List<dynamic> projects = data["patterns"];
+
+                        var fbResponse;
+
+                        ImagePicker picker = new ImagePicker();
+                        File file = await ImagePicker.pickImage(
+                            source: ImageSource.camera);
+                        // SocialShare.shareFacebookStory(file.path, "#ffffff",
+                        //         "#000000", "https://deep-link-url")
+                        //     .then((value) => fbResponse = value);
+
+                        // await SocialSharePlugin.shareToFeedFacebook(
+                        //     path: file.path);
+
+                        // for (int i; i < data["patterns"])
+                        int d = 2;
+                      },
+                      child: const Text('Add To Project',
+                          style: TextStyle(fontSize: 20)),
+                    )),
+                    Expanded(
+                        child: RaisedButton(
                       color: Colors.blueAccent,
                       onPressed: () {},
-                      child: const Text('Upload Photo', style: TextStyle(fontSize: 20)),
-                    )
-                ),
-      ]
-    ),
-
-    )
-        )
-    );
+                      child: const Text('Upload Photo',
+                          style: TextStyle(fontSize: 20)),
+                    )),
+                  ]),
+            )));
   }
 }
-
