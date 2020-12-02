6e502cbf68895db3643ed91ef6242524fe2bb687
Tihandewet <50331253+Tihandewet@users.noreply.github.com>
2020-12-02 09:56:24 +0200

    Ui Changes

--- knitwit_feature_module/lib/main.dart
+++ knitwit_feature_module/lib/main.dart
@@ -10,58 +10,51 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      service.fetchAlbum();
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("KnitWit Module Test"),
         ),
+        body:
+        Container(
+          child:
+          Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+        Expanded(
+          child: RaisedButton(
+            color: Colors.white38,
+              onPressed: () {},
+          child: const Text('Share To Facebook', style: TextStyle(fontSize: 20)),
+        )
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+                Expanded(
+                    child: RaisedButton(
+                      color: Colors.lightBlue,
+                      onPressed: () {},
+
+                      child: const Text('Search Ravelry', style: TextStyle(fontSize: 20)),
+                    )
+                ),
+                Expanded(
+                    child: RaisedButton(
+                      color: Colors.cyan,
+                      onPressed: () {},
+                      child: const Text('Add To Project', style: TextStyle(fontSize: 20)),
+                    )
+                ),
+                Expanded(
+                    child: RaisedButton(
+                      color: Colors.blueAccent,
+                      onPressed: () {},
+                      child: const Text('Upload Photo', style: TextStyle(fontSize: 20)),
+                    )
+                ),
+      ]
+    ),
+
+    )
+        )
     );
   }
 }
+
